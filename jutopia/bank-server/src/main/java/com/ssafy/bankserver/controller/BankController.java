94e5b04cce2cfb2e441f21d4a1ad27925efea00b
신성주 <hans52410537@gmail.com>
2023-09-18 17:53:22 +0900

    📝Fix: fix yaml file

---
+++ jutopia/bank-server/src/main/java/com/ssafy/bankserver/controller/BankController.java
@@ -0,0 +1,25 @@
+package com.ssafy.bankserver.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/bank-service")
+public class BankController {
+    Environment env;
+
+    @Autowired
+    public BankController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return String.format("It's working in Order Service on PORT %s", env.getProperty("local.server.port"));
+    }
+
+
+}
