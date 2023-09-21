54d440bac42714bf4dfdc7091cfc63f8c902a7d6
신성주 <hans52410537@gmail.com>
2023-09-21 17:58:09 +0900

    📝Feat: get bank all, class bank

---
+++ jutopia/bank-server/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into bank(bank_name, class_id, consumption_tax_rate, capital_gains_tax_rate, exchange_tax_rate, money, trade_tax_rate)
+    values('하나은행', 'b4e318ed-2892-4b5c-9cf1-e1b25e45886d', 0.5, 0.2, 0.18, 100000, 0.29);
+
+insert into bank(bank_name, class_id, consumption_tax_rate, capital_gains_tax_rate, exchange_tax_rate, money, trade_tax_rate)
+    values('국민은행', 'bas318ed-2892-4b5c-9cf1-e1b25e45886d', 0.5, 0.2, 0.18, 200000, 0.25);
+
+
+insert into bank(bank_name, class_id, consumption_tax_rate, capital_gains_tax_rate, exchange_tax_rate, money, trade_tax_rate)
+    values('IBK기업은행', 'b4e318ed-4412-4b5c-9cf1-e1b25e45886d', 0.12, 0.23, 0.48, 300000, 0.11);
+
+
+insert into bank(bank_name, class_id, consumption_tax_rate, capital_gains_tax_rate, exchange_tax_rate, money, trade_tax_rate)
+    values('신한은행', 'b4e318ed-1123-4b5c-9cf1-e1b25e45886d', 0.11, 0.26, 0.13, 400000, 0.19);
