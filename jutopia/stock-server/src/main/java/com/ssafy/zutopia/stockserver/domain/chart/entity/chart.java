51b2d3a86c6f32ae68a06a5acb1dba0b9e0c35e0
Young Seob So <9412sys@gmail.com>
2023-09-25 17:17:29 +0900

    📰feat: create entity

---
+++ jutopia/stock-server/src/main/java/com/ssafy/zutopia/stockserver/domain/chart/entity/chart.java
@@ -0,0 +1,30 @@
+package com.ssafy.zutopia.stockserver.domain.chart.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Entity
+public class chart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String 캔들의시간크기;
+    private LocalDateTime 타임스탬프;
+    private BigDecimal 시가;
+    private BigDecimal 고가;
+    private BigDecimal 저가;
+    private BigDecimal 종가;
+    private Long 거래량;
+    private BigDecimal 거래대금;
+
+}
---
+++ jutopia/stock-server/src/main/java/com/ssafy/zutopia/stockserver/domain/volume/entity/Volume.java
@@ -0,0 +1,31 @@
+package com.ssafy.zutopia.stockserver.domain.volume.entity;
+
+import com.ssafy.zutopia.stockserver.domain.stock.entity.Stock;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@Entity
+public class Volume {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime 샘플의시간크기;
+    private LocalDateTime timestamp;
+    private Long 외인보유량;
+    private Long 외인순매수;
+    private Long 기간보유량;
+    private Long 기관보유비율;
+    private Long 기관순매수량;
+    private Long 개인보유량;
+    private Long 개인보유비율;
+    private Long 개인순매수량;
+    private Long 총거래량;
+    private BigDecimal 종가;
+}
