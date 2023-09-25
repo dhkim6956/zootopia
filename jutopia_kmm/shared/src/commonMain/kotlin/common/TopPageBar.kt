1054ecdee446091f4a72e4fa89d61c5bbe6f3270
dhkim6956 <dhkim6956@naver.com>
2023-09-26 02:55:43 +0900

    📝Feat: return button

--- jutopia_kmm/shared/src/commonMain/kotlin/common/TopPageBar.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/common/TopPageBar.kt
@@ -7,13 +7,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.KeyboardArrowLeft
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import icehimchanFontFamily
+import moe.tlaster.precompose.navigation.Navigator
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -28,9 +34,22 @@ fun TopPageBar(navLoc: String) {
             .fillMaxWidth()
             .height(80.dp)
             .background(color = Variables.ColorsBackground)
-            .padding(start = 40.dp, top = 16.dp, end = 40.dp, bottom = 16.dp)
+            .padding(start = 20.dp, top = 16.dp, end = 40.dp, bottom = 16.dp)
     ) {
-        Text(navLoc, fontFamily = icehimchanFontFamily, fontSize = 28.sp)
+        Row (
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = {},
+            ) {
+                Icon(
+                    Icons.Outlined.KeyboardArrowLeft,
+                    "뒤로가기"
+                )
+            }
+            Text(navLoc, fontFamily = icehimchanFontFamily, fontSize = 28.sp)
+        }
         Image(
             painterResource("drawable/text_bubble.xml"),
             null,
