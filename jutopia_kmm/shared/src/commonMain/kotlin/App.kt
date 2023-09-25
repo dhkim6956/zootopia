9ba7204710866c212f296bd353422c37ba689899
im jun hwan <dlawnsghks12@naver.com>
2023-09-25 14:02:30 +0900

    📰Feat: Creat menu button

--- jutopia_kmm/shared/src/commonMain/kotlin/App.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/App.kt
@@ -1,13 +1,18 @@
 import androidx.compose.animation.EnterTransition
-import androidx.compose.animation.ExitTransition
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import asset.Asset
+import home.Bank
 import moe.tlaster.precompose.navigation.NavHost
 import moe.tlaster.precompose.navigation.rememberNavigator
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import home.Home
+import home.Market
+import home.Notice
+import home.Rent
+import home.Stock
+import home.Trade
 import menus.Menus
 import moe.tlaster.precompose.navigation.transition.NavTransition
 import news.News
@@ -47,6 +52,36 @@ fun App() {
                 route = "/menus"
             ) {
                 Menus(navigator)
+            }
+            scene(
+                route = "/bank"
+            ) {
+                Bank(navigator)
+            }
+            scene(
+                route = "/stock"
+            ) {
+                Stock(navigator)
+            }
+            scene(
+                route = "/rent"
+            ) {
+                Rent(navigator)
+            }
+            scene(
+                route = "/trade"
+            ) {
+                Trade(navigator)
+            }
+            scene(
+                route = "/market"
+            ) {
+                Market(navigator)
+            }
+            scene(
+                route = "/notice"
+            ) {
+                Notice(navigator)
             }
         }
     }
