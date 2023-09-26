ebf7a60f9e15b81642cfc8539bda4bfc7063db4d
김현종 <dw3510@naver.com>
2023-09-26 13:18:24 +0900

    Feat : 주식 차트 페이지 구성

--- jutopia_kmm/shared/src/commonMain/kotlin/App.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/App.kt
@@ -3,16 +3,27 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import asset.Asset
+import co.touchlab.kermit.Logger
+import home.Bank
 import home.Home
+import home.Market
+import home.Notice
+import home.Rent
+import home.Stock
+import home.Trade
 import lease.LeaseScreen
 import menus.Menus
 import moe.tlaster.precompose.navigation.NavHost
-import moe.tlaster.precompose.navigation.RouteBuilder
+import moe.tlaster.precompose.navigation.path
 import moe.tlaster.precompose.navigation.rememberNavigator
 import moe.tlaster.precompose.navigation.transition.NavTransition
 import news.News
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import school.School
+import stock.stockchart.StockChartScreen
+import stock.stocklist.StockListScreen
+
+private val log = Logger.withTag("App")
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -35,6 +46,31 @@ fun App() {
                 Asset(navigator)
             }
             scene(
+                route = "/asset/deposit"
+            ) {
+                Asset(navigator)
+            }
+            scene(
+                route = "/asset/save"
+            ) {
+                Asset(navigator)
+            }
+            scene(
+                route = "/asset/point"
+            ) {
+                Asset(navigator)
+            }
+            scene(
+                route = "/asset/stock"
+            ) {
+                Asset(navigator)
+            }
+            scene(
+                route = "/asset/building"
+            ) {
+                Asset(navigator)
+            }
+            scene(
                 route = "/school"
             ) {
                 School(navigator)
@@ -50,9 +86,51 @@ fun App() {
                 Menus(navigator)
             }
             scene(
+                route = "/bank"
+            ) {
+                Bank(navigator)
+            }
+            scene(
+                route = "/stock"
+            ) {
+                Stock(navigator)
+            }
+            scene(
+                route = "/rent"
+            ) {
+                Rent(navigator)
+            }
+            scene(
+                route = "/trade"
+            ) {
+                Trade(navigator)
+            }
+            scene(
+                route = "/market"
+            ) {
+                Market(navigator)
+            }
+            scene(
+                route = "/notice"
+            ) {
+                Notice(navigator)
+            }
+            scene(
                 route = "/lease"
             ) {
                 LeaseScreen(navigator)
+            }
+            scene(
+                route = "/stocklist"
+            ){
+                StockListScreen(navigator)
+            }
+            scene(
+                route = "/stockChart/{stockId}?"
+            ) {backStackEntry ->
+                log.i { "---------------------------------" }
+                val stockId: String? = backStackEntry.path<String>("stockId")
+                StockChartScreen(stockId!!, navigator)
             }
         }
     }
--- jutopia_kmm/shared/src/commonMain/kotlin/asset/Asset.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/asset/Asset.kt
@@ -1,15 +1,132 @@
+@file:OptIn(ExperimentalMaterialApi::class, ExperimentalMaterialApi::class,
+    ExperimentalMaterialApi::class, ExperimentalResourceApi::class
+)
+
 package asset
 
 import BottomTabBar
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.ChipDefaults
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FilterChip
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
 import common.TopPageBar
 import moe.tlaster.precompose.navigation.Navigator
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun Asset(navigator: Navigator) {
+
+
+    var kotTest:String = "lalala"
+
+
+
     Column {
         TopPageBar("자산")
-        BottomTabBar(navigator)
+
+        Chips(navigator)
+    }
+    BottomTabBar(navigator)
+}
+
+@Composable
+fun Chips(navigator: Navigator ) {
+
+    Row (
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        FilterChip(
+            onClick = { navigator.navigate("/asset/deposit") },
+            selected = true,
+            colors = ChipDefaults.filterChipColors(
+                backgroundColor = Color(0xFFF1D3FB),
+                contentColor = Color(0xFFAF30C9)
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource("drawable/chip_deposit.xml"),
+                    contentDescription = "chip_deposit"
+                )
+            }
+        ) {
+            Text("입출금")
+        }
+        FilterChip(
+            onClick = { navigator.navigate("/asset/save") },
+            selected = true,
+            colors = ChipDefaults.filterChipColors(
+                backgroundColor = Color(0xFFDFDBF9),
+                contentColor = Color(0xFF7E51D6)
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource("drawable/chip_save.xml"),
+                    contentDescription = "chip_save"
+                )
+            }
+        ) {
+            Text("적금")
+        }
+        FilterChip(
+            onClick = { navigator.navigate("/asset/point") },
+            selected = true,
+            colors = ChipDefaults.filterChipColors(
+                backgroundColor = Color(0xFFCBD8F2),
+                contentColor = Color(0xFF4963C7)
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource("drawable/chip_point.xml"),
+                    contentDescription = "chip_point"
+                )
+            }
+        ) {
+            Text("포인트")
+        }
+        FilterChip(
+            onClick = { navigator.navigate("/asset/stock") },
+            selected = true,
+            colors = ChipDefaults.filterChipColors(
+                backgroundColor = Color(0xFFB7E7FF),
+                contentColor = Color(0xFF0087D1)
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource("drawable/chip_stock.xml"),
+                    contentDescription = "chip_stock"
+                )
+            }
+        ) {
+            Text("주식")
+        }
+        FilterChip(
+            onClick = { navigator.navigate("/asset/building") },
+            selected = true,
+            colors = ChipDefaults.filterChipColors(
+                backgroundColor = Color(0xFFC8EAC9),
+                contentColor = Color(0xFF358438)
+            ),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource("drawable/chip_building.xml"),
+                    contentDescription = "chip_building"
+                )
+            }
+        ) {
+            Text("부동산")
+        }
     }
 }
