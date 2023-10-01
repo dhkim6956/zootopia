84f417c5fad60eea1982f0e77c7d497cfc29cbd4
bellring <starringbell@gmail.com>
2023-10-01 22:31:19 +0900

    Fix : swagger

--- jutopia/rent-server/src/main/java/com/ssafy/rentserver/config/SwaggerConfig.java
+++ jutopia/rent-server/src/main/java/com/ssafy/rentserver/config/SwaggerConfig.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.core.jackson.ModelResolver;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -32,4 +37,15 @@ public class SwaggerConfig {
     public ModelResolver modelResolver(ObjectMapper objectMapper){
         return new ModelResolver(objectMapper);
     }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("/"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new io.swagger.v3.oas.models.info.Info().title("springdoc API").version("V1")
+                        .license(new License().name("Apache 2.0").url("<http://springdoc.org>")));
+    }
 }
+
+
