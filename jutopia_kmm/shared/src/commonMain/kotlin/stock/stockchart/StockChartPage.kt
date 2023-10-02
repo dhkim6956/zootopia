63ab3cc8c13168356058bd714e839abe8a0dda13
bellring <starringbell@gmail.com>
2023-10-02 19:56:42 +0900

    Feat : bar 차트 테스트

--- jutopia_kmm/shared/src/commonMain/kotlin/stock/stockchart/StockChartPage.kt
+++ jutopia_kmm/shared/src/commonMain/kotlin/stock/stockchart/StockChartPage.kt
@@ -1,6 +1,8 @@
 package stock.stockchart
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
@@ -18,9 +23,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import com.aay.compose.barChart.BarChart
+import com.aay.compose.barChart.model.BarParameters
+import com.aay.compose.baseComponents.model.GridOrientation
+import com.aay.compose.donutChart.PieChart
+import com.aay.compose.donutChart.model.PieChartData
 import com.aay.compose.lineChart.LineChart
 import com.aay.compose.lineChart.model.LineParameters
 import com.aay.compose.lineChart.model.LineType
@@ -53,9 +68,10 @@ fun StockChartPage(
         Box(
             modifier = Modifier.height(350.dp)
         ){
-            chart(
-                chartData, clickedPoints,
-            )
+//            chart(
+//                chartData, clickedPoints,
+//            )
+            BarChartSample(chartData)
         }
         Button(
             onClick = {
@@ -63,7 +79,6 @@ fun StockChartPage(
             },
             modifier = Modifier
                 .fillMaxWidth()
-
         ) {
             Text("주문")
         }
@@ -78,28 +93,34 @@ fun chart(
     clickedPoints: MutableList<Pair<Float, Float>>,
     modifier: Modifier = Modifier
 ) {
-    val latestData = chartData.takeLast(10)
+    val latestData = chartData.takeLast(15)
+
     val lineParameters: List<LineParameters> = listOf(
         LineParameters(
-            label = "Stock Price",
+            label = "주식 가격",
             data = latestData.map { it.second },
             lineColor = Color.Gray,
-            lineType = LineType.CURVED_LINE,
-            lineShadow = true,
+            lineType = LineType.DEFAULT_LINE,
+            lineShadow = false,
         )
 
     )
 
-    val xAxisData = chartData.map { (it.first.toString()) }
+    val xAxisData = latestData.map { (it.first) }
 
 
-    Box {
+    Box(Modifier.fillMaxSize()) {
         LineChart(
             modifier = Modifier
-                .fillMaxWidth(),
+                .border(2.dp, color = Color.Magenta),
             linesParameters = lineParameters,
+            isGrid = true,
             xAxisData = xAxisData,
-
+            showGridWithSpacer = true,
+            yAxisRange = 10,
+            oneLineChart = false,
+            gridOrientation = GridOrientation.HORIZONTAL,
+            animateChart = true,
             )
         DisplayClickedPointInfo(clickedPoints)
     }
@@ -112,6 +133,39 @@ fun DisplayClickedPointInfo(clickedPoints: List<Pair<Float, Float>>) {
         val lastPoint = clickedPoints.last()
         val (x, y) = lastPoint
 
-        Text("Clicked Point: x = $x, y = $y")
+        Text("시간 = $x, 가격 = $y")
+    }
+}
+@Composable
+fun BarChartSample(chartData: List<Pair<String, Double>>) {
+
+    val testBarParameters: List<BarParameters> = listOf(
+        BarParameters(
+            dataName = "주식 가격",
+            data = chartData.map { it.second },
+            barColor = Color(0xFF6C3428)
+        ),
+    )
+
+    Box(Modifier.fillMaxSize().padding(bottom = 5.dp)) {
+        BarChart(
+            chartParameters = testBarParameters,
+            gridColor = Color.DarkGray,
+            xAxisData = chartData.map{it.first},
+            isShowGrid = true,
+            animateChart = true,
+            showGridWithSpacer = true,
+            yAxisStyle = TextStyle(
+                fontSize = 14.sp,
+                color = Color.DarkGray,
+            ),
+            xAxisStyle = TextStyle(
+                fontSize = 14.sp,
+                color = Color.DarkGray,
+                fontWeight = FontWeight.W400
+            ),
+            yAxisRange = 15,
+            barWidth = 13.dp,
+        )
     }
 }
