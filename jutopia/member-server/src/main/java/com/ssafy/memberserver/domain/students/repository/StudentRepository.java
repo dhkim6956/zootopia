de4cf2450ad698b7bf526ad65e1b23ba7ab08998
신성주 <hans52410537@gmail.com>
2023-10-03 01:59:42 +0900

    📝Fix: user login

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/repository/StudentRepository.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/repository/StudentRepository.java
@@ -12,11 +12,6 @@ import java.util.UUID;
 public interface StudentRepository extends JpaRepository<Student, UUID> {
     Optional<Student> findByStudentId(String studentId);
     boolean existsByStudentId(String studentId);
-<<<<<<< jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/repository/StudentRepository.java
-    List<Student> findBySchoolAndGradeAndClassRoom(String school,int grade,int classRoom);
-    Optional<Student> findByStudentIdAndMemberRole(String memberId, MemberRole memberRole);
-=======
     List<Student> findAllByClassroomId(UUID classroomId);
     Optional<Student> findByStudentIdAndMemberRole(String studentId, MemberRole memberRole);
->>>>>>> jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/students/repository/StudentRepository.java
 }
