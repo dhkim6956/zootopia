c42d8d4df053e0aa3f566902e907541a452a26e7
소영섭 <9412sys@gmail.com>
2023-10-04 02:09:44 +0900

    fix: notice and history

--- jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/notice/dto/response/NoticeResponse.java
+++ jutopia/member-server/src/main/java/com/ssafy/memberserver/domain/notice/dto/response/NoticeResponse.java
@@ -2,21 +2,26 @@ package com.ssafy.memberserver.domain.notice.dto.response;
 
 import com.ssafy.memberserver.common.enums.NoticeStatus;
 import com.ssafy.memberserver.domain.notice.entity.Notice;
+import lombok.Builder;
 
+import java.time.LocalDateTime;
+@Builder
 public record NoticeResponse(
         Long id,
         String title,
         String content,
         Long viewCount,
+        LocalDateTime createdAt,
         NoticeStatus noticeStatus
 ) {
     public static NoticeResponse from(Notice notice){
-        return new NoticeResponse(
-                notice.getId(),
-                notice.getTitle(),
-                notice.getContent(),
-                notice.getViewCount(),
-                notice.getNoticeStatus()
-        );
+        return NoticeResponse.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .viewCount(notice.getViewCount())
+                .createdAt(notice.getCreatedAt())
+                .noticeStatus(notice.getNoticeStatus())
+                .build();
     }
 }
