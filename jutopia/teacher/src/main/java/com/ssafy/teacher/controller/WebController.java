dd9925411ecae5917b33a3dadc3cf1c6e6f675c6
김현종 <dw3510@naver.com>
2023-10-05 17:35:31 +0900

    Fix : 선생님 페이지 경로 수정

--- jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
+++ jutopia/teacher/src/main/java/com/ssafy/teacher/controller/WebController.java
@@ -13,12 +13,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigDecimal;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/teacher")
 public class WebController {
 
     private final TeacherService service;
